00aae022fff1274921539ba0e059b1962494de1d
deiamcha <dk.eiamchan@gmail.com>
2025-05-01 09:30:17 +0700

    backgroundrandomcolor

--- cell03/ex00/script.js
+++ cell03/ex00/script.js
@@ -1,3 +1,10 @@
-let changebtn = document.querySelector("button")
-changebtn.addEventListener("click",() => {document.body.style.backgroundColor = generateRandomColor();})
-function generateRandomColor () {let RandomColor = "#" + Math.floor(Math.random() * 16777215) . toString(16);return RandomColor;}
+let changebtn = document.querySelector("button");
+
+changebtn.addEventListener("click",function(){
+    document.body.style.backgroundColor = generateRandomColor();
+})
+
+function generateRandomColor () {
+    let RandomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return RandomColor;
+}
